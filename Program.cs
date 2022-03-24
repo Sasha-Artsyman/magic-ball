479a004485eafe8daed6bfe3a38161ea38633577
Sasha Artsyman <65505084+Sasha-Artsyman@users.noreply.github.com>
2022-03-24 21:10:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ask a question and press 'Enter'");
+        Console.WriteLine(" (Press key 'End' to complete)  \n");
+
+        bool end0 = false;
+        string[] answers0 = { "Yes", "No", "Probably yes", "Probably no", "Maybe" };
+        
+        do
+        {
+            Console.Write("Your question: ");
+            Console.ReadLine();
+            Console.WriteLine("Answer: " + answers0[new Random().Next(0, answers0.Length)]);
+            
+            if (Console.ReadKey(true).Key == ConsoleKey.End)
+            {
+                end0 = true;
+            }
+        }
+        while (!end0);
+
+        Console.ReadLine();
+    }
+}
